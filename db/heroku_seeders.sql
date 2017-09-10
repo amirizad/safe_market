498608fa9b38283297ea8f8cbe0908cda6b0d008
amir <info@amirizad.com>
2017-09-10 13:08:05 -0700

    Heroku seeders updated

--- db/heroku_seeders.sql
+++ db/heroku_seeders.sql
@@ -1,13 +1,13 @@
-INSERT INTO `heroku_f8ff56462b848ef`.`users`
+INSERT INTO `heroku_d97e12e6d4f9baa`.`users`
 (`username`,`email`,`password`,`role`,`age`,`zip`,`phone`,`user_image_url`,`fname`,`lname`,`createdAt`, `updatedAt`)
 VALUES('test1', 'test1@test1.com', '$2a$10$ehLyvD.vTC1fTZD5JfKMAu2lyv/Paqwf2KLYhD.7e6b2FqrcAq2OC', 'user', '34', '92691', '5625633567', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first1','last1','2017-08-01 12:00:00','2017-08-01 12:00:00'),
 ('test2', 'test21@test2.com', '$2a$10$d888l6s6bdlZ9edj6gli7.uNwQuQIUJaStupmp7CYiuJmEp1Ny7RS', 'user', '25', '92688', '5625633567', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first2','last2','2017-08-01 12:00:00','2017-08-01 12:00:00'),
 ('test3', 'test3@test3.com', '$2a$10$ehLyvD.vTC1fTZD5JfKMAu2lyv/Paqwf2KLYhD.7e6b2FqrcAq2OC', 'user', '52', '92692', '2135633567', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first3','last3','2017-08-01 12:00:00','2017-08-01 12:00:00'),
 ('test4', 'test4@test4.com', '$2a$10$ehLyvD.vTC1fTZD5JfKMAu2lyv/Paqwf2KLYhD.7e6b2FqrcAq2OC', 'user', '44', '92692', '9495111567', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first4','last4','2017-08-01 12:00:00','2017-08-01 12:00:00'),
-('test5', 'test5@test5.com', '$2a$10$ehLyvD.vTC1fTZD5JfKMAu2lyv/Paqwf2KLYhD.7e6b2FqrcAq2OC', 'user', '19', '92694', '714563222', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first5','last5','2017-08-01 12:00:00','2017-08-01 12:00:00');
-('Amir', 'info@amirizad.com', '$2a$10$062/xXKkL83U7/Fsq11OkObmsjG2y98FY4rszELfzORomOifV970W', 'user', '38', '92692', '9494197049', 'https://www.dropbox.com/s/yr6zt3qn0xr5s9g/Amir1.jpg?raw=1','','','2017-08-01 12:00:00','2017-08-01 12:00:00');
+('test5', 'test5@test5.com', '$2a$10$ehLyvD.vTC1fTZD5JfKMAu2lyv/Paqwf2KLYhD.7e6b2FqrcAq2OC', 'user', '19', '92694', '714563222', 'https://cdn.filestackcontent.com/pHtjny1tQqOY44A0A40s','first5','last5','2017-08-01 12:00:00','2017-08-01 12:00:00'),
+('Amir', 'info@amirizad.com', '$2a$10$062/xXKkL83U7/Fsq11OkObmsjG2y98FY4rszELfzORomOifV970W', 'user', '38', '92692', '9494197049', 'https://www.dropbox.com/s/yr6zt3qn0xr5s9g/Amir1.jpg?raw=1','Amir','Izadpanah','2017-08-01 12:00:00','2017-08-01 12:00:00');
 
-INSERT INTO `heroku_f8ff56462b848ef`.`items`
+INSERT INTO `heroku_d97e12e6d4f9baa`.`items`
 (`title`,`description`,`category`,`item_image_url`,`quantity`,`price`,`unit_type`,`barter_ind`,`buyer_sale_confirm`,`seller_sale_confirm`,`seller_rating`,`UserId`,`createdAt`, `updatedAt`)
 VALUES('phone','apple iphone 7','Electronics',null,1,50,'item flat',0,0,1,null,1,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 ('lawnmower','honda lawnmower 2010','Equipment',null,1,100,'item flat',0,1,1,4,1,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
@@ -17,7 +17,7 @@ VALUES('phone','apple iphone 7','Electronics',null,1,50,'item flat',0,0,1,null,1
 ('42'' TV','Samsung HDTV 42 inch 2017','Electronics',null,1,200,'item flat',0,0,1,null,3,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 ('handy man','Reliable handyman good quality work ','Servies',null,2,40,'per hour',0,0,0,null,3,'2017-08-24 12:00:00','2017-08-24 12:00:00');
 
-INSERT INTO `heroku_f8ff56462b848ef`.`offers`
+INSERT INTO `heroku_d97e12e6d4f9baa`.`offers`
 (`ItemId`,`offer_amt`,`barter_ItemId`,`offer_accepted_ind`,`offer_accepted_dtm`,`UserId`,`createdAt`, `updatedAt`)
 VALUES(1,50,null,0,null,4,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 (2,90,null,0,null,5,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
@@ -30,7 +30,7 @@ VALUES(1,50,null,0,null,4,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 (6,200,null,1,'2017-08-24 23:00:00',4,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 (5,50,null,0,null,4,'2017-08-24 12:00:00','2017-08-24 12:00:00');
 
-INSERT INTO `heroku_f8ff56462b848ef`.`messages`
+INSERT INTO `heroku_d97e12e6d4f9baa`.`messages`
 (`ItemId`,`FromId`,`ToId`,`message_text`,`read_ind`,`createdAt`, `updatedAt`)
 VALUES(2,5,1,'Will you take less than asking',1,'2017-08-24 12:00:00','2017-08-24 12:00:00'),
 (2,1,5,'No',1,'2017-08-24 12:00:00','2017-08-24 12:01:00'),
@@ -43,44 +43,44 @@ VALUES(2,5,1,'Will you take less than asking',1,'2017-08-24 12:00:00','2017-08-2
 (6,2,3,'Do you have original box?',1,'2017-08-24 12:00:00','2017-08-24 12:08:00'),
 (7,4,3,'Do you do plaster repairs?',1,'2017-08-24 12:00:00','2017-08-24 12:09:00');
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set buyer_sale_confirm=0, seller_sale_confirm=0
 where id<>2;
 
-update heroku_f8ff56462b848ef.offers
+update heroku_d97e12e6d4f9baa.offers
 set offer_accepted_ind=0, offer_accepted_dtm=null
 where itemId<>0;
 
-Update heroku_f8ff56462b848ef.items 
+Update heroku_d97e12e6d4f9baa.items 
 set item_image_url="http://lorempixel.com/output/sports-q-c-640-480-5.jpg"
 where id=3;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set item_image_url="http://lorempixel.com/output/technics-q-c-640-480-7.jpg"
 where id=1;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 Set item_image_url="https://thumb9.shutterstock.com/display_pic_with_logo/79181/131401952/stock-photo-red-lawn-mower-131401952.jpg"
 where id=2;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set item_image_url="http://lorempixel.com/output/fashion-q-c-640-480-3.jpg"
 where id=5;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set item_image_url="https://thumb1.shutterstock.com/display_pic_with_logo/680755/288200936/stock-photo-yukata-and-futon-of-japan-288200936.jpg"
 where id=4;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set item_image_url="http://lorempixel.com/output/technics-q-c-640-480-9.jpg"
 where id=6;
 
-update heroku_f8ff56462b848ef.items
+update heroku_d97e12e6d4f9baa.items
 set item_image_url="https://thumb9.shutterstock.com/display_pic_with_logo/151216/264151898/stock-photo-handyman-with-tools-264151898.jpg"
 where id=7;
 
 
-INSERT INTO `heroku_f8ff56462b848ef`.`items`
+INSERT INTO `heroku_d97e12e6d4f9baa`.`items`
 (`title`,`description`,`category`,`item_image_url`,`quantity`,`price`,`unit_type`,`barter_ind`,`buyer_sale_confirm`,`seller_sale_confirm`,`seller_rating`,`UserId`,`createdAt`, `updatedAt`)
 VALUES('Beats Headphones', 'Solo 3 Wireless', 'Electronics', 'https://images-na.ssl-images-amazon.com/images/I/41fnjLZS5BL.jpg', '1', '100.00', 'item flat', '0', '0', '0', NULL, 1, '2017-08-24 12:00:00', '2017-08-24 12:00:00'),
 ('Power Washer', 'Sun Joe SPX4000 like new', 'Equipment', 'https://i5.walmartimages.com/asr/f83efaf5-f338-4588-a0f6-d2698df1dd2b_1.fd2c400fb6ace07d9bdcf4a315e029f2.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF', 1, '90.00', 'item flat', '0', '0', '0', NULL, 1,'2017-08-24 12:00:00', '2017-08-24 12:00:00'),
@@ -90,7 +90,7 @@ VALUES('Beats Headphones', 'Solo 3 Wireless', 'Electronics', 'https://images-na.
 ('Refurbished IPad', 'iPad 2 32GB WiFi', 'Electronics', 'https://cdn3.bigcommerce.com/s-ndqvi4q/products/268/images/1040/zambezi-ipad234_black-standard__43881.1434157444.500.659.jpg?c=2', '1', '148.00', 'item flat', '0', '0', '0', NULL, 4, '2017-08-24 12:02:00', '2017-08-24 12:00:00'),
 ('Plumber', 'Apprentice plumber cheap drain clear', 'Servies', 'https://thumbs.dreamstime.com/b/plumber-crack-142669.jpg', '2', '35.00', 'per hour', '0', '0', '0', NULL, 5, '2017-08-24 12:01:00', '2017-08-24 12:00:00');
 
-use `heroku_f8ff56462b848ef`;
+use `heroku_d97e12e6d4f9baa`;
 Update `users`
 set fname='first1', lname='lname1'
 where id=1;
